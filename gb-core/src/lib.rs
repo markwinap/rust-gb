4bda40a935ff45257105609c2548f2b355d59fc5
plozano <plozano@24hourfit.com>
2021-01-26 15:03:23 -0800

    Reorder stuff

---
+++ gb-core/src/lib.rs
@@ -0,0 +1,14 @@
+mod cpu;
+mod hardware;
+mod memory;
+mod util;
+pub mod gameboy;
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,4 @@
-
-mod cpu;
-mod hardware;
-mod memory;
-//mod util;
-mod util;
-mod gameboy;
-
-extern crate phf;
-extern crate num_traits;
-#[macro_use]
-extern crate enum_display_derive;
-
-#[macro_use]
-extern crate bitflags;
-
-#[macro_use]
-extern crate num_derive;
-
+extern crate gb_core;
 fn main() {
     println!("Hello, world!");
 }
